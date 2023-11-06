a41adca7eee4730aa12e3c069be0c2961fa13c1c
GoorezyEST <goorezyestudios@gmail.com>
2023-11-06 19:28:22 -0300

    Page wrapper

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
 "use client";
 
+import BackgroundDecoration from "@/components/layouts/BackgroundDecoration";
+import Hero from "@/components/layouts/Hero";
 import styles from "@/styles/modules/wrapper.module.css";
 
 export default function Wrapper() {
-  return <main>Hello</main>;
+  return (
+    <main className={styles.wrapper}>
+      <Hero />
+      {/* <BackgroundDecoration /> */}
+    </main>
+  );
 }
