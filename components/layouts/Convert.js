bcaaade59709ddd0338c8493c19625335b510489
GoorezyEST <goorezyestudios@gmail.com>
2023-11-06 20:09:48 -0300

    Let layouts created

---
+++ components/layouts/Convert.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "@/styles/modules/convert.module.css";
+
+function Convert() {
+  return (
+    <section className={styles.container}>
+      <span style={{ color: "red" }}>Featured table here</span>
+    </section>
+  );
+}
+
+export default Convert;
---
+++ components/layouts/Trending.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "@/styles/modules/trending.module.css";
+
+function Trending() {
+  return (
+    <section className={styles.container}>
+      <span style={{ color: "red" }}>Featured table here</span>
+    </section>
+  );
+}
+
+export default Trending;
