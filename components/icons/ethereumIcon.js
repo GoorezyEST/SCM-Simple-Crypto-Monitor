85f7c2e6a48ba01c3ff83b00c03da4024d84bfa7
GoorezyEST <goorezyestudios@gmail.com>
2023-11-06 16:48:36 -0300

    Created all icons

---
+++ components/icons/ethereumIcon.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+function EthereumIcon() {
+  return (
+    <svg
+      width="21"
+      height="32"
+      viewBox="0 0 21 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_448_60)">
+        <path
+          d="M10.2828 0L10.0681 0.729237V21.8881L10.2828 22.1023L20.1043 16.2967L10.2828 0Z"
+          fill="#fff"
+        />
+        <path
+          d="M10.2828 0L0.46106 16.2967L10.2828 22.1023V11.8324V0Z"
+          fill="#fff"
+        />
+        <path
+          d="M10.2829 23.9619L10.1619 24.1094V31.6465L10.2829 31.9997L20.1104 18.1593L10.2829 23.9619Z"
+          fill="#fff"
+        />
+        <path
+          d="M10.2828 31.9997V23.9619L0.46106 18.1593L10.2828 31.9997Z"
+          fill="#fff"
+        />
+        <path
+          d="M10.2828 22.1023L20.1044 16.2967L10.2828 11.8324V22.1023Z"
+          fill="#fff"
+        />
+        <path
+          d="M0.46106 16.2967L10.2828 22.1023V11.8324L0.46106 16.2967Z"
+          fill="#fff"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_448_60">
+          <rect
+            width="19.6493"
+            height="32"
+            fill="white"
+            transform="translate(0.46106)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default EthereumIcon;
