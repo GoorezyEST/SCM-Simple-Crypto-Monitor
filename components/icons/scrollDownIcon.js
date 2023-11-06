c86ccd48b103426812317e45279efa601c91833a
GoorezyEST <goorezyestudios@gmail.com>
2023-11-06 19:28:09 -0300

    Created missing icon

---
+++ components/icons/scrollDownIcon.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ScrollDownIcon() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="16" cy="16" r="15" stroke="#78F05A" strokeWidth="2" />
+      <path
+        d="M15.2929 24.7071C15.6834 25.0976 16.3166 25.0976 16.7071 24.7071L23.0711 18.3431C23.4616 17.9526 23.4616 17.3195 23.0711 16.9289C22.6805 16.5384 22.0474 16.5384 21.6569 16.9289L16 22.5858L10.3431 16.9289C9.95262 16.5384 9.31946 16.5384 8.92893 16.9289C8.53841 17.3195 8.53841 17.9526 8.92893 18.3431L15.2929 24.7071ZM15 8L15 24H17V8H15Z"
+        fill="#78F05A"
+      />
+    </svg>
+  );
+}
+
+export default ScrollDownIcon;
