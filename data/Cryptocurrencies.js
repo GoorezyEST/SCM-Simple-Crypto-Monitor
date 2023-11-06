56ef472e48165edd85798657e6d9d98d17539345
GoorezyEST <goorezyestudios@gmail.com>
2023-11-06 20:10:15 -0300

    All the cryptos data

---
+++ data/Cryptocurrencies.js
@@ -0,0 +1,10 @@
+export const CryptosData = [
+  { id: "bitcoin", symbol: "btc", name: "Bitcoin" },
+  { id: "ethereum", symbol: "eth", name: "Ethereum" },
+  { id: "tether", symbol: "usdt", name: "Tether" },
+  { id: "usd-coin", symbol: "usdc", name: "USDC" },
+  { id: "binancecoin", symbol: "bnb", name: "BNB" },
+  { id: "solana", symbol: "sol", name: "Solana" },
+  { id: "cardano", symbol: "ada", name: "Cardano" },
+  { id: "ripple", symbol: "xrp", name: "XRP" },
+];
