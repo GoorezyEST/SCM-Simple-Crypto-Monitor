4307cb55411581466882aef4a9114a2726d57368
GoorezyEST <goorezyestudios@gmail.com>
2023-11-06 20:10:28 -0300

    Added all new components to the main wrapper

--- app/page.js
+++ app/page.js
@@ -1,13 +1,20 @@
 "use client";
 
+import React from "react";
 import BackgroundDecoration from "@/components/layouts/BackgroundDecoration";
 import Hero from "@/components/layouts/Hero";
 import styles from "@/styles/modules/wrapper.module.css";
+import Trending from "@/components/layouts/Trending";
+import Convert from "@/components/layouts/Convert";
+import Footer from "@/components/units/Footer";
 
 export default function Wrapper() {
   return (
     <main className={styles.wrapper}>
       <Hero />
+      <Trending />
+      <Convert />
+      <Footer />
       {/* <BackgroundDecoration /> */}
     </main>
   );
