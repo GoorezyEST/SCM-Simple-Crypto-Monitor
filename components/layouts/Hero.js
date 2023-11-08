5882ed9ce4c1f1420a5d053ad534cde1dc57e50d
GoorezyEST <goorezyestudios@gmail.com>
2023-11-08 19:30:41 -0300

    Modified Navbar and added translations

--- components/layouts/Hero.js
+++ components/layouts/Hero.js
@@ -2,8 +2,26 @@ import React from "react";
 import SCVLogo from "../icons/scvLogo";
 import styles from "@/styles/modules/hero.module.css";
 import ScrollDownIcon from "../icons/scrollDownIcon";
+import { useGlobal } from "@/context/GlobalContext";
+import { CurrenciesList } from "@/data/Currencies";
 
 function Hero() {
+  const {
+    lang,
+    langSettings,
+    swapLanguage,
+    currentCurrency,
+    setCurrentCurrency,
+  } = useGlobal();
+
+  const changeLang = () => {
+    if (lang === "es") {
+      swapLanguage("en");
+    } else {
+      swapLanguage("es");
+    }
+  };
+
   return (
     <section className={styles.container}>
       <nav className={styles.nav}>
@@ -11,28 +29,59 @@ function Hero() {
           <SCVLogo />
         </div>
         <ul>
-          <li>Home</li>
-          <li>Featured</li>
-          <li>Convert</li>
+          <li>{langSettings.nav.one}</li>
+          <li>{langSettings.nav.two}</li>
+          <li>{langSettings.nav.three}</li>
         </ul>
-        <span>Get in touch</span>
+        <div className={styles.side}>
+          <ul className={styles.currencies}>
+            <li>
+              {currentCurrency.toUpperCase()}
+              <ul className={styles.sub_currencies}>
+                {CurrenciesList.map((item) => {
+                  if (item !== currentCurrency) {
+                    return (
+                      <li
+                        key={item}
+                        onClick={() => {
+                          setCurrentCurrency(item);
+                        }}
+                      >
+                        {item.toUpperCase()}
+                      </li>
+                    );
+                  }
+                })}
+              </ul>
+            </li>
+          </ul>
+          <span
+            className={styles.language}
+            onClick={() => {
+              changeLang();
+            }}
+          >
+            {langSettings.nav.four}
+          </span>
+          <span className={styles.contact}>{langSettings.nav.five}</span>
+        </div>
       </nav>
 
       <div className={styles.content}>
         <div className={styles.text}>
-          <span>SIMPLE CRYPTO VIEWER</span>
+          <span>{langSettings.hero.one}</span>
           <h1>
-            Your simple gateway
+            {langSettings.hero.two}
             <br />
-            to the crypto world
+            {langSettings.hero.three}
           </h1>
           <p>
-            Keep track of the most trending cryptocurrencies and preview
+            {langSettings.hero.four}
             <br />
-            conversions between a wide range of currencies.
+            {langSettings.hero.five}
           </p>
         </div>
-        <button className={styles.cta}>Get started</button>
+        <button className={styles.cta}>{langSettings.hero.six}</button>
       </div>
 
       <div className={styles.scroll}>
