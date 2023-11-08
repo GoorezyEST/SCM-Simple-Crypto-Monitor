c5a976a60fbb1e033ab1ca6e8fdb6219b8314717
GoorezyEST <goorezyestudios@gmail.com>
2023-11-08 19:29:48 -0300

    Created initial function that fetches data and then toggles the loading screen

--- app/page.js
+++ app/page.js
@@ -1,14 +1,37 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import BackgroundDecoration from "@/components/layouts/BackgroundDecoration";
 import Hero from "@/components/layouts/Hero";
 import styles from "@/styles/modules/wrapper.module.css";
 import Trending from "@/components/layouts/Trending";
 import Convert from "@/components/layouts/Convert";
 import Footer from "@/components/units/Footer";
+import { useGlobal } from "@/context/GlobalContext";
+import { getCryptos } from "@/functions/APILogic";
 
 export default function Wrapper() {
+  //Import from the global context
+  const { setIsHydrated, setCryptoInformation } = useGlobal();
+
+  const initFunction = async () => {
+    try {
+      //Get all cryptos information
+      const cryptoData = await getCryptos();
+      //Store the information in the global context
+      setCryptoInformation(cryptoData);
+      //Set the loading state to false
+      setIsHydrated(false);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
+
+  //Call the init function
+  useEffect(() => {
+    initFunction();
+  }, []);
+
   return (
     <main className={styles.wrapper}>
       <Hero />
