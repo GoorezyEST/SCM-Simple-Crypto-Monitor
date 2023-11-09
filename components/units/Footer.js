e73883eea15c3cfe7e278b0c165872c24924e35a
GoorezyEST <goorezyestudios@gmail.com>
2023-11-09 12:43:31 -0300

    Updated footer

--- components/units/Footer.js
+++ components/units/Footer.js
@@ -3,14 +3,57 @@ import styles from "@/styles/modules/footer.module.css";
 import SCVLogo from "../icons/scvLogo";
 import Link from "next/link";
 import { useGlobal } from "@/context/GlobalContext";
+import gsap from "gsap";
+import ScrollToPlugin from "gsap/dist/ScrollToPlugin";
 
 function Footer() {
   const { langSettings } = useGlobal();
 
+  gsap.registerPlugin(ScrollToPlugin);
+
+  const scrollToHome = () => {
+    const section = document.querySelector("#home-section");
+
+    gsap.to(window, {
+      duration: 1.25,
+      scrollTo: section,
+      ease: "power1.in",
+    });
+  };
+
+  const scrollToTrending = () => {
+    const section = document.querySelector("#trending-section");
+
+    gsap.to(window, {
+      duration: 1.25,
+      scrollTo: section,
+      ease: "power1.in",
+    });
+  };
+
+  const scrollToConvert = () => {
+    const section = document.querySelector("#convert-section");
+
+    gsap.to(window, {
+      duration: 1.25,
+      scrollTo: section,
+      ease: "power1.in",
+    });
+  };
+
   return (
-    <footer className={styles.container}>
-      <div className={styles.logo}>
-        <SCVLogo />
+    <footer className={styles.container} id="footer">
+      <div className={styles.start}>
+        <div className={styles.logo}>
+          <SCVLogo />
+        </div>
+        <div className={styles.navigation}>
+          <ul>
+            <li onClick={scrollToHome}>{langSettings.nav.one}</li>
+            <li onClick={scrollToTrending}>{langSettings.nav.two}</li>
+            <li onClick={scrollToConvert}>{langSettings.nav.three}</li>
+          </ul>
+        </div>
       </div>
 
       <div className={styles.line}></div>
