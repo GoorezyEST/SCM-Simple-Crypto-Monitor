f3203b1207b357ae110ea52eece4156f27764d91
GoorezyEST <goorezyestudios@gmail.com>
2023-11-10 17:01:57 -0300

    Deleted navbar from this section

--- components/layouts/Hero.js
+++ components/layouts/Hero.js
@@ -1,43 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "@/styles/modules/hero.module.css";
 import ScrollDownIcon from "../icons/scrollDownIcon";
 import { useGlobal } from "@/context/GlobalContext";
-import Navbar from "../units/Navbar";
-import { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import gsap from "gsap";
 import ScrollToPlugin from "gsap/dist/ScrollToPlugin";
 
-const variants = {
-  visible: { opacity: 1, y: 0 },
-  hide: { opacity: 0, y: -100 },
-};
-
 function Hero() {
   const { langSettings } = useGlobal();
-
-  const [showNav, setShowNav] = useState(true);
-  const lastScrollPosition = useRef(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPosition = window.scrollY;
-      if (currentScrollPosition > lastScrollPosition.current) {
-        if (showNav) {
-          setShowNav(false);
-        }
-      } else {
-        if (!showNav) {
-          setShowNav(true);
-        }
-      }
-      lastScrollPosition.current = currentScrollPosition;
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [showNav]);
 
   gsap.registerPlugin(ScrollToPlugin);
 
@@ -53,33 +23,55 @@ function Hero() {
 
   return (
     <section className={styles.container} id="home-section">
-      <motion.div
-        className={styles.navContainer}
-        animate={showNav ? "visible" : "hide"}
-        variants={variants}
-        transition={{
-          duration: 0.35,
-        }}
-      >
-        <Navbar />
-      </motion.div>
       <div className={styles.content}>
         <div className={styles.text}>
-          <span>{langSettings.hero.one}</span>
-          <h1>
+          <motion.span
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              duration: 0.25,
+              delay: 0.125,
+            }}
+          >
+            {langSettings.hero.one}
+          </motion.span>
+          <motion.h1
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              duration: 0.25,
+              delay: 0.125,
+            }}
+          >
             {langSettings.hero.two}
             <br />
             {langSettings.hero.three}
-          </h1>
-          <p>
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              duration: 0.25,
+              delay: 0.125,
+            }}
+          >
             {langSettings.hero.four}
             <br />
             {langSettings.hero.five}
-          </p>
+          </motion.p>
         </div>
-        <button className={styles.cta} onClick={scrollToTrending}>
+        <motion.button
+          className={styles.cta}
+          onClick={scrollToTrending}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{
+            duration: 0.25,
+            delay: 0.125,
+          }}
+        >
           {langSettings.hero.six}
-        </button>
+        </motion.button>
       </div>
 
       <div className={styles.scroll}>
