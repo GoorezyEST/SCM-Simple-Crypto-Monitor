d902d97a7cff4fc7afe6ae8f8d59dc2d3235f6ad
GoorezyEST <goorezyestudios@gmail.com>
2023-11-10 17:01:20 -0300

    Modified fill value into CSS variable

--- components/icons/scvLogo.js
+++ components/icons/scvLogo.js
@@ -11,15 +11,15 @@ function SCVLogo() {
     >
       <path
         d="M316 273H0L29 223H230L217 200H43L158 0L232 128H175L158 100L129 151H246L316 273Z"
-        fill="white"
+        fill="var(--white)"
       />
       <path
         d="M183 0L341 273L423 130H366L341 172L270 49H412L395 79H453L499 0H183Z"
-        fill="white"
+        fill="var(--white)"
       />
       <path
         d="M524 0L366 273H424L524 100L537 121.5L450 273H507L565.5 172L579 194L533 273H591L636 194L524 0Z"
-        fill="white"
+        fill="var(--white)"
       />
       <path d="M648.5 216L616 273H680L648.5 216Z" fill="#5ADEF0" />
     </svg>
