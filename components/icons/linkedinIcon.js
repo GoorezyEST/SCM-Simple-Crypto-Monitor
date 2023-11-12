edb43c7751a2569eb4f4cb1e3fcb62562e29fae0
GoorezyEST <goorezyestudios@gmail.com>
2023-11-12 17:07:39 -0300

    Added social media icons

---
+++ components/icons/linkedinIcon.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function LinkedInIcon() {
+  return (
+    <svg
+      width="64"
+      height="64"
+      viewBox="0 0 64 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_472_2)">
+        <path
+          d="M8.91125 56.6H18.0238V22.8838H8.91125V56.6ZM13.4675 8C10.43 8 8 10.43 8 13.4675C8 16.505 10.43 18.935 13.4675 18.935C16.505 18.935 18.935 16.505 18.935 13.4675C18.935 10.43 16.505 8 13.4675 8ZM33.515 28.0475V22.8838H24.4025V56.6H33.515V39.2863C33.515 29.5663 45.9688 28.9588 45.9688 39.2863V56.6H55.0813V35.945C55.0813 19.5425 37.7675 20.15 33.515 28.0475Z"
+          fill="var(--green)"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_472_2">
+          <rect width="64" height="64" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default LinkedInIcon;
---
+++ components/icons/portfolioIcon.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function PortfolioIcon() {
+  return (
+    <svg
+      width="64"
+      height="64"
+      viewBox="0 0 64 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M52 18H46.8C44.4 12 38.6 8 32 8C25.4 8 19.6 12 17.2 18H12C8.6 18 6 20.6 6 24V25.2C6 32.2 11.8 38 18.8 38H45.4C52.4 38 58.2 32.2 58.2 25.2V24C58 20.6 55.4 18 52 18ZM32 12C36.4 12 40.2 14.4 42.4 18H21.6C23.8 14.4 27.6 12 32 12Z"
+        fill="var(--green)"
+      />
+      <path
+        d="M46 42V46C46 47.2 45.2 48 44 48C42.8 48 42 47.2 42 46V42H22V46C22 47.2 21.2 48 20 48C18.8 48 18 47.2 18 46V42C13.2 41.8 9 39.6 6 36.2V50C6 53.4 8.6 56 12 56H52C55.4 56 58 53.4 58 50V36.2C55 39.6 50.8 41.8 46 42Z"
+        fill="var(--green)"
+      />
+    </svg>
+  );
+}
+
+export default PortfolioIcon;
