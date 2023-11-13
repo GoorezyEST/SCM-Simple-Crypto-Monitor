ae789f38bb79abce6646c3932b2243c6f6772684
GoorezyEST <goorezyestudios@gmail.com>
2023-11-13 18:10:50 -0300

    Navbar responsive design done

--- components/units/Navbar.js
+++ components/units/Navbar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "@/styles/modules/navbar.module.css";
 import SCVLogo from "../icons/scvLogo";
 import { CurrenciesList } from "@/data/Currencies";
 import gsap from "gsap";
 import ScrollToPlugin from "gsap/dist/ScrollToPlugin";
 import { useGlobal } from "@/context/GlobalContext";
+import HamburgerIcon from "../icons/hamburgerIcon";
+import CloseIcon from "../icons/closeIcon";
 
 function Navbar() {
   const {
@@ -13,6 +15,8 @@ function Navbar() {
     swapLanguage,
     currentCurrency,
     setCurrentCurrency,
+    mobileMenuOpen,
+    setMobileMenuOpen,
   } = useGlobal();
 
   const changeLang = () => {
@@ -119,6 +123,58 @@ function Navbar() {
         <span className={styles.contact} onClick={scrollToContact}>
           {langSettings.nav.five}
         </span>
+      </div>
+      <div className={styles.mobile}>
+        <div
+          className={styles.mobile_icon}
+          onClick={() => {
+            setMobileMenuOpen((prev) => !prev);
+          }}
+        >
+          {mobileMenuOpen ? <CloseIcon /> : <HamburgerIcon />}
+        </div>
+        <div
+          className={styles.mobile_menu}
+          style={{ transform: mobileMenuOpen ? "scale(1)" : "scale(0)" }}
+        >
+          <h6>Menu</h6>
+          <ul>
+            <li onClick={scrollToHome}>{langSettings.nav.one}</li>
+            <li onClick={scrollToTrending}>{langSettings.nav.two}</li>
+            <li onClick={scrollToConvert}>{langSettings.nav.three}</li>
+          </ul>
+          <ul>
+            {CurrenciesList.map((item) => {
+              return (
+                <li
+                  style={{
+                    color:
+                      currentCurrency.toUpperCase() === item.toUpperCase()
+                        ? "var(--green)"
+                        : "",
+                  }}
+                  key={item}
+                  onClick={() => {
+                    setCurrentCurrency(item);
+                  }}
+                >
+                  {item.toUpperCase()}
+                </li>
+              );
+            })}
+          </ul>
+          <span
+            className={styles.mobile_language}
+            onClick={() => {
+              changeLang();
+            }}
+          >
+            {langSettings.nav.four}
+          </span>
+          <span className={styles.mobile_contact} onClick={scrollToContact}>
+            {langSettings.nav.five}
+          </span>
+        </div>
       </div>
     </nav>
   );
