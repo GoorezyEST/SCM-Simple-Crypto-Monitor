5b681c475bf8cfd359cf73eea2c561830c27861a
GoorezyEST <goorezyestudios@gmail.com>
2023-11-13 18:10:08 -0300

    Added new icons for navbar in mobile

---
+++ components/icons/closeIcon.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function CloseIcon() {
+  return (
+    <svg
+      width="64"
+      height="64"
+      viewBox="0 0 64 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M48 48L32 32M32 32L16 16M32 32L48 16M32 32L16 48"
+        stroke="var(--white)"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default CloseIcon;
---
+++ components/icons/hamburgerIcon.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function HamburgerIcon() {
+  return (
+    <svg
+      width="64"
+      height="64"
+      viewBox="0 0 64 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M53.3335 16H10.6668M53.3335 32H26.6668M53.3335 48H40.0002"
+        stroke="var(--white)"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default HamburgerIcon;
